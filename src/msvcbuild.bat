724ce58b81add5d8bff4e3638dd25b182a061e6d
ThePixelMoon <yourlocalmoon7@gmail.com>
2025-10-19 11:16:01 +0300

    enforce static library builds in msvcbuild.bat

--- src/msvcbuild.bat
+++ src/msvcbuild.bat
@@ -1,25 +1,19 @@
 @rem Script to build LuaJIT with MSVC.
 @rem Copyright (C) 2005-2025 Mike Pall. See Copyright Notice in luajit.h
 @rem
-@rem Open a "Visual Studio Command Prompt" (either x86 or x64).
-@rem Then cd to this directory and run this script. Use the following
-@rem options (in order), if needed. The default is a dynamic release build.
+@rem MODIFIED: Force true static library builds
 @rem
-@rem   nogc64        disable LJ_GC64 mode for x64
-@rem   debug         emit debug symbols
-@rem   lua52compat   enable extra Lua 5.2 extensions
-@rem   amalg         amalgamated build
-@rem   static        create static lib to statically link into your project
-@rem   mixed         create static lib to build a DLL in your project
 
 @if not defined INCLUDE goto :FAIL
 
 @setlocal
-@rem Add more debug flags here, e.g. DEBUGCFLAGS=/DLUA_USE_ASSERT
+@rem Add more debug flags here, e.g. DLUA_USE_ASSERT
 @set DEBUGCFLAGS=
-@set LJCOMPILE=cl /nologo /c /O2 /W3 /D_CRT_SECURE_NO_DEPRECATE /D_CRT_STDIO_INLINE=__declspec(dllexport)__inline
+@set LJCOMPILE=cl /nologo /c /O2 /W3 /D_CRT_SECURE_NO_DEPRECATE
 @set LJDYNBUILD=/DLUA_BUILD_AS_DLL /MD
 @set LJDYNBUILD_DEBUG=/DLUA_BUILD_AS_DLL /MDd 
+@set LJSTATIC=/MT
+@set LJSTATIC_DEBUG=/MTd
 @set LJCOMPILETARGET=/Zi
 @set LJLINKTYPE=/DEBUG /RELEASE
 @set LJLINKTYPE_DEBUG=/DEBUG
@@ -99,7 +93,7 @@ buildvm -m folddef -o lj_folddef.h lj_opt_fold.c
 @if "%1" neq "debug" goto :NODEBUG
 @shift
 @set LJCOMPILE=%LJCOMPILE% %DEBUGCFLAGS%
-@set LJDYNBUILD=%LJDYNBUILD_DEBUG%
+@set LJSTATIC=%LJSTATIC_DEBUG%
 @set LJLINKTYPE=%LJLINKTYPE_DEBUG%
 :NODEBUG
 @if "%1" neq "lua52compat" goto :NOLUA52COMPAT
@@ -108,40 +102,51 @@ buildvm -m folddef -o lj_folddef.h lj_opt_fold.c
 :NOLUA52COMPAT
 @set LJCOMPILE=%LJCOMPILE% %LJCOMPILETARGET%
 @set LJLINK=%LJLINK% %LJLINKTYPE% %LJLINKTARGET%
-@if "%1"=="amalg" goto :AMALGDLL
+
 @if "%1"=="static" goto :STATIC
+@if "%1"=="amalg" goto :AMALGDLL
 %LJCOMPILE% %LJDYNBUILD% lj_*.c lib_*.c
 @if errorlevel 1 goto :BAD
-@if "%1"=="mixed" goto :STATICLIB
 %LJLINK% /DLL /OUT:%LJDLLNAME% lj_*.obj lib_*.obj
 @if errorlevel 1 goto :BAD
 @goto :MTDLL
+
 :STATIC
-%LJCOMPILE% lj_*.c lib_*.c
+%LJCOMPILE% %LJSTATIC% lj_*.c lib_*.c
 @if errorlevel 1 goto :BAD
-:STATICLIB
-%LJLIB% /OUT:%LJLIBNAME% lj_*.obj lib_*.obj
+%LJLIB% /OUT:%LJLIBNAME% lj_*.obj lib_*.obj lj_vm.obj
 @if errorlevel 1 goto :BAD
-@goto :MTDLL
+@del *.obj *.manifest minilua.exe buildvm.exe
+@del host\buildvm_arch.h
+@del lj_bcdef.h lj_ffdef.h lj_libdef.h lj_recdef.h lj_folddef.h
+@echo.
+@echo === Successfully built LuaJIT STATIC library for Windows/%LJARCH% ===
+@goto :END
+
 :AMALGDLL
 @if "%2"=="static" goto :AMALGSTATIC
 %LJCOMPILE% %LJDYNBUILD% ljamalg.c
 @if errorlevel 1 goto :BAD
-@if "%2"=="mixed" goto :AMALGSTATICLIB
 %LJLINK% /DLL /OUT:%LJDLLNAME% ljamalg.obj lj_vm.obj
 @if errorlevel 1 goto :BAD
 @goto :MTDLL
 :AMALGSTATIC
-%LJCOMPILE% ljamalg.c
+%LJCOMPILE% %LJSTATIC% ljamalg.c
 @if errorlevel 1 goto :BAD
-:AMALGSTATICLIB
 %LJLIB% /OUT:%LJLIBNAME% ljamalg.obj lj_vm.obj
 @if errorlevel 1 goto :BAD
+@del *.obj *.manifest minilua.exe buildvm.exe
+@del host\buildvm_arch.h
+@del lj_bcdef.h lj_ffdef.h lj_libdef.h lj_recdef.h lj_folddef.h
+@echo.
+@echo === Successfully built LuaJIT STATIC library for Windows/%LJARCH% ===
+@goto :END
+
 :MTDLL
 if exist %LJDLLNAME%.manifest^
   %LJMT% -manifest %LJDLLNAME%.manifest -outputresource:%LJDLLNAME%;2
 
-%LJCOMPILE% luajit.c
+%LJCOMPILE% %LJDYNBUILD% luajit.c
 @if errorlevel 1 goto :BAD
 %LJLINK% /OUT:luajit.exe luajit.obj %LJLIBNAME%
 @if errorlevel 1 goto :BAD
@@ -165,7 +170,7 @@ if exist luajit.exe.manifest^
 @echo.
 @echo *******************************************************
 @echo *** Build FAILED -- Please check the error messages ***
-@echo *******************************************************
+@echo *******************************************************
 @goto :END
 :FAIL
 @echo You must open a "Visual Studio Command Prompt" to run this script
